698be8a38c055ecec9af71244cf57615e436212c
Eric Larson <eclarson@smu.edu>
2024-10-02 09:02:10 -0500

    adding more model view controller mechanisms to demonstration

---
+++ Commotion/MotionModel.swift
@@ -0,0 +1,69 @@
+//
+//  MotionModel.swift
+//  Commotion
+//
+//  Created by Eric Cooper Larson on 10/2/24.
+//  Copyright © 2024 Eric Larson. All rights reserved.
+//
+
+import CoreMotion
+
+// setup a protocol for the ViewController to be delegate for
+protocol MotionDelegate {
+    // Define delegate functions
+    func activityUpdated(activity:CMMotionActivity)
+    func pedometerUpdated(pedData:CMPedometerData)
+}
+
+class MotionModel{
+    
+    // MARK: =====Class Variables=====
+    let activityManager = CMMotionActivityManager()
+    let pedometer = CMPedometer()
+    var delegate:MotionDelegate? = nil
+    
+    // MARK: =====Motion Methods=====
+    func startActivityMonitoring(){
+        // is activity is available
+        if CMMotionActivityManager.isActivityAvailable(){
+            // update from this queue (should we use the MAIN queue here??.... )
+            self.activityManager.startActivityUpdates(to: OperationQueue.main)
+            {(activity:CMMotionActivity?)->Void in
+                // unwrap the activity and send to delegate
+                // using the real time pedometer might influences how often we get activity updates...
+                // so these updates can come through less often than we may want
+                if let unwrappedActivity = activity,
+                   let delegate = self.delegate {
+                    // Print if we are walking or running
+                    print("%@",unwrappedActivity.description)
+                    
+                    // Call delegate function
+                    delegate.activityUpdated(activity: unwrappedActivity)
+                    
+                }
+            }
+        }
+        
+    }
+    
+    func startPedometerMonitoring(){
+        // check if pedometer is okay to use
+        if CMPedometer.isStepCountingAvailable(){
+            // start updating the pedometer from the current date and time
+            pedometer.startUpdates(from: Date())
+            {(pedData:CMPedometerData?, error:Error?)->Void in
+                
+                // if no errors, update the delegate
+                if let unwrappedPedData = pedData,
+                   let delegate = self.delegate {
+                    
+                    delegate.pedometerUpdated(pedData:unwrappedPedData)
+                }
+
+            }
+        }
+    }
+    
+    
+}
+
--- Commotion/ViewController.swift
+++ Commotion/ViewController.swift
@@ -9,11 +9,9 @@
 import UIKit
 import CoreMotion
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
     
-    // MARK: =====Class Variables=====
-    let activityManager = CMMotionActivityManager()
-    let pedometer = CMPedometer()
+    let motionModel = MotionModel()
 
     // MARK: =====UI Outlets=====
     @IBOutlet weak var activityLabel: UILabel!
@@ -25,55 +23,37 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        self.startActivityMonitoring()
-        self.startPedometerMonitoring()
-    }
-
-
-    // MARK: =====Motion Methods=====
-    func startActivityMonitoring(){
-        // is activity is available
-        if CMMotionActivityManager.isActivityAvailable(){
-            // update from this queue (should we use the MAIN queue here??.... )
-            self.activityManager.startActivityUpdates(to: OperationQueue.main)
-            {(activity:CMMotionActivity?)->Void in
-                // unwrap the activity and display
-                // using the real time pedometer influences how often we get activity updates...
-                // so these updates can come through less often than we may want
-                if let unwrappedActivity = activity {
-                    // Print if we are walking or running
-                    print("%@",unwrappedActivity.description)
-                    self.activityLabel.text = "🚶: \(unwrappedActivity.walking), 🏃: \(unwrappedActivity.running)"
-                }
-            }
-        }
+        self.motionModel.delegate = self
         
+        self.motionModel.startActivityMonitoring()
+        self.motionModel.startPedometerMonitoring()
     }
+
+
     
-    func startPedometerMonitoring(){
-        // check if pedometer is okay to use
-        if CMPedometer.isStepCountingAvailable(){
-            // start updating the pedometer from the current date and time
-            pedometer.startUpdates(from: Date())
-            {(pedData:CMPedometerData?, error:Error?)->Void in
-                
-                // if no errors, update the main UI
-                if let data = pedData {
-                    
-                    // display the output directly on the phone
-                    DispatchQueue.main.async {
-                        // this goes into the large gray area on view
-                        self.debugLabel.text = "\(data.description)"
-                        
-                        // this updates the progress bar with number of steps, assuming 100 is the maximum for the steps
-                        
-                        self.progressBar.progress = data.numberOfSteps.floatValue / 100
-                    }
-                }
-            }
-        }
-    }
 
 
 }
 
+extension ViewController: MotionDelegate{
+    // MARK: =====Motion Delegate Methods=====
+    
+    func activityUpdated(activity:CMMotionActivity){
+        
+        self.activityLabel.text = "🚶: \(activity.walking), 🏃: \(activity.running)"
+
+    }
+    
+    func pedometerUpdated(pedData:CMPedometerData){
+
+        // display the output directly on the phone
+        DispatchQueue.main.async {
+            // this goes into the large gray area on view
+            self.debugLabel.text = "\(pedData.description)"
+            
+            // this updates the progress bar with number of steps, assuming 100 is the maximum for the steps
+            
+            self.progressBar.progress = pedData.numberOfSteps.floatValue / 100
+        }  
+    }
+}
